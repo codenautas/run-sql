e43df3b02eb65b33f72d8c4dee06dfb8c39395b4
Emilio Platzer <emilioplatzer@gmail.com>
2016-04-02 19:34:45 -0400

    adapto al transpiler

--- install/transpile.js
+++ install/transpile.js
@@ -2,11 +2,20 @@
 
 var entrypoint = {"path": "lib", "name": "run-sql.js"};
 
+var entrypoints=[
+    {"path": "lib", "name": "sql-tools.js"},
+    entrypoint
+];
+
 var Path = require('path');
 
-console.log('transpiling...',entrypoint.name);
-
-require('es6-transpiler').run({
-    filename: Path.dirname(__dirname)+'/'+entrypoint.path+'/'+entrypoint.name,
-    outputFilename: Path.dirname(__dirname)+'/'+entrypoint.path+'-trans/'+entrypoint.name
+entrypoints.forEach(function(entrypoint){
+    console.log('transpiling...',entrypoint.name);
+    require('es6-transpiler').run({
+        filename: Path.dirname(__dirname)+'/'+entrypoint.path+'/'+entrypoint.name,
+        outputFilename: Path.dirname(__dirname)+'/'+entrypoint.path+'-trans/'+entrypoint.name
+    });
 });
+
+
+
